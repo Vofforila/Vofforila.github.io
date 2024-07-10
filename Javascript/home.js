902cc8d4adb6983904a7e5c6d36bc545d1e6e7cb
Vofforila <polberci@yahoo.ro>
2024-07-10 09:19:47 +0300

    update

--- Javascript/home.js
+++ Javascript/home.js
@@ -5,7 +5,7 @@ const popup_text = document.getElementsByClassName("popup-text")[0];
 function Popup(_button_id) {
    if (_button_id == "IT-Teams") {
       popup_text.innerHTML =
-         "Worked in IT Teams Flutter Software Development. Had to develop functionality and database realations for diffrent customers. I learned how to use and structure a website using containers and how to manimulate data using Firebase NoSQL.";
+         "Worked in IT Teams Flutter Software Development. Had to develop functionality and database relations for different customers. I learned how to use and structure a website using containers and how to manipulate data using Firebase NoSQL.";
    }
    popup.style.display = "block";
 }
