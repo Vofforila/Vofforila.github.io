653cdd745676ccf1520e689453b45a3f37a3fee4
Vofforila <polberci@yahoo.ro>
2024-09-16 20:56:16 +0300

    update

--- Javascript/home.js
+++ Javascript/home.js
@@ -3,10 +3,17 @@ var span = document.getElementsByClassName("close")[0];
 const popup_text = document.getElementsByClassName("popup-text")[0];
 
 function Popup(_button_id) {
-   if (_button_id == "IT-Teams") {
-      popup_text.innerHTML =
-         "Worked in IT Teams Flutter Software Development. Had to develop functionality and database relations for different customers. I learned how to use and structure a website using containers and how to manipulate data using Firebase NoSQL.";
+   switch (_button_id) {
+      case "IT-Teams":
+         popup_text.innerHTML =
+            "Worked in IT Teams Flutter Software Development. Had to develop functionality and database relations for different customers. I learned how to use and structure a website using containers and how to manipulate data using Firebase NoSQL.";
+         break;
+      case "PRIME-Telecom":
+         popup_text.innerHTML =
+            "Worked in PRIME Telecom as a support operator. Had to answer phone calls and read email and create tickets and update based on the progress. Also had to check server and devices status via bash commands.";
+         break;
    }
+
    popup.style.display = "block";
 }
 
