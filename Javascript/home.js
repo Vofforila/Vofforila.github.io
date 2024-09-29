2f82934e4bfaed8b49eb262e2b3ad0004c1e8f6b
Vofforila <polberci@yahoo.ro>
2024-09-29 16:15:01 +0300

    update

--- Javascript/home.js
+++ Javascript/home.js
@@ -6,11 +6,15 @@ function Popup(_button_id) {
    switch (_button_id) {
       case "IT-Teams":
          popup_text.innerHTML =
-            "Worked in IT Teams Flutter Software Development. Had to develop functionality and database relations for different customers. I learned how to use and structure a website using containers and how to manipulate data using Firebase NoSQL.";
+            "Worked at IT Teams as a Flutter Software Development. Developed functionality and database relationships for various clients. Learned how to structure a website using containers and manipulate data with Firebase NoSQL.";
          break;
       case "PRIME-Telecom":
          popup_text.innerHTML =
-            "Worked in PRIME Telecom as a support operator. Had to answer phone calls and read email and create tickets and update based on the progress. Also had to check server and devices status via bash commands.";
+            "Worked at PRIME Telecom as a Support Operator. Managed customer inquiries via phone and email, created and updated tickets based on progress, and monitored server and device statuses using Bash commands.";
+         break;
+      case "Lima-Credit":
+         popup_text.innerHTML =
+            "Worked at LIMA CREDIT IFN SRL as a System Administrator. Designed and configured the computer network while managing hardware and software installations for pawnshop operations. I learned to oversee software licenses, administer the network, and maintain employee computers effectively.";
          break;
    }
 
@@ -26,3 +30,12 @@ window.onclick = function (event) {
       popup.style.display = "none";
    }
 };
+
+var side_panel = document.getElementById("side-panel");
+function openPanel() {
+   side_panel.classList.add("open");
+}
+
+function closePanel() {
+   side_panel.classList.remove("open");
+}
